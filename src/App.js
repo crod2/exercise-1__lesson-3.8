22b99a2bc5b16dba58236095cafbc911e8a24474
crod2 <carlarodriguez90@gmail.com>
2019-05-13 22:17:58 +0200

    Get language info

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ class App extends React.Component {
     super(props);
     this.state = {
       name: '',
-      description: ''
+      description: '',
+      language: ''
     }
     this.changeName = this.changeName.bind(this);
     this.changeDescription = this.changeDescription.bind(this);
+    this.changeLanguage = this.changeLanguage.bind(this);
   }
 
   changeName(event) {
@@ -17,6 +19,10 @@ class App extends React.Component {
 
   changeDescription(event) {
     this.setState( {description: event.currentTarget.value} )
+  }
+
+  changeLanguage(event) {
+    this.setState ( {language: event.currentTarget.value} )
   }
 
   render() {
@@ -36,10 +42,16 @@ class App extends React.Component {
           value={this.state.description}
           onChange={this.changeDescription}
           />
+          <select value={this.state.language} onChange={this.changeLanguage}>
+            <option value="Español">Español</option>
+            <option value="Inglés">Inglés</option>
+            <option value="Portugués">Portugués</option>
+          </select>
         </form>
         <div>
           <h1 onChange={this.changeName}>{this.state.name}</h1>
           <h2 onChange={this.changeDescription}>{this.state.description}</h2>
+          <h3 onChange={this.changeLanguage}>{this.state.language}</h3>
         </div>
       </div>
       
