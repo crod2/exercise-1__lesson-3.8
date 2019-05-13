6d9713068e95b670aa6cfeb34147264bf2cf096d
crod2 <carlarodriguez90@gmail.com>
2019-05-13 22:02:35 +0200

    Get name and description on window

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: ''
+      name: '',
+      description: ''
     }
     this.changeName = this.changeName.bind(this);
+    this.changeDescription = this.changeDescription.bind(this);
   }
 
   changeName(event) {
     this.setState( {name: event.currentTarget.value} )
+  }
+
+  changeDescription(event) {
+    this.setState( {description: event.currentTarget.value} )
   }
 
   render() {
@@ -24,9 +30,16 @@ class App extends React.Component {
           value={this.state.name}
           onChange={this.changeName}
           />
+          <label htmlFor="description">Sinopsis:</label>
+          <textarea
+          id="description"
+          value={this.state.description}
+          onChange={this.changeDescription}
+          />
         </form>
         <div>
           <h1 onChange={this.changeName}>{this.state.name}</h1>
+          <h2 onChange={this.changeDescription}>{this.state.description}</h2>
         </div>
       </div>
       
