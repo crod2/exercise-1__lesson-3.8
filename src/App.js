774dd60407485500c7edbfe859be1f548b318e78
crod2 <carlarodriguez90@gmail.com>
2019-05-13 21:54:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    }
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName(event) {
+    this.setState( {name: event.currentTarget.value} )
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="name">Nombre:</label>
+          <input
+          id="name"
+          type="text"
+          value={this.state.name}
+          onChange={this.changeName}
+          />
+        </form>
+        <div>
+          <h1 onChange={this.changeName}>{this.state.name}</h1>
+        </div>
+      </div>
+      
+    )
+  }
 }
 
 export default App;
